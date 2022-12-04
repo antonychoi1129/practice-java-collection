b7c84f3bcf39ee5bf462a03666817e7640a749d5
Antony Choi <antony.choi@oocl.com>
2022-12-04 15:38:07 +0800

    all

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,14 +5,19 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream()
+            .filter(number -> number % 2 == 1)
+            .reduce((accumulated, oddNumber) -> oddNumber)
+            .get();
     }
 
     public String getLongest(List<String> words) {
-        return null;
+        return words.stream().
+            reduce("", (accumulated, current) -> current.length() > accumulated.length()?current:accumulated);
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream()
+            .reduce(0, (accumulated, current) -> accumulated + current.length(), Integer::sum);
     }
 }
